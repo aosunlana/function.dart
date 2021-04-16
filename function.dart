ebce8eabdfee0df1925c605d0580bed81258489c
developerBarak <81052783+developerBarak@users.noreply.github.com>
2021-04-16 23:09:18 +0100

    Create function.dart

---
+++ function.dart
@@ -0,0 +1,46 @@
+// void main() {
+// //   calling the first function
+//   function1(10, 10);
+//   print(function1(10, 10));
+  
+  
+// //   calling the second function
+//   function2(500, 250);
+  
+  
+// //   calling the third function
+//     //   multiplying any two numbers
+//   print(function3(num1: 2, num3: 6));
+//     //   multiplying any three numbers
+//   print(function3(num1: 2, num3: 6, num4: 8));
+//     //   multiplying all four numbers
+//   print(function3(num2: 4, num1: 2, num3: 6, num4: 8));
+  
+  
+// //   calling the forth function
+//   printName(name: "developerBarak");
+// }
+
+
+
+// // A function with two arguments and returns the addition of both arguments.
+// int function1(int a, int b) {
+//   return a + b;
+// }
+
+// // A function with two arguments, prints the division of both arguments but returns no value.
+// void function2(int a, int b) {
+//   var c = a / b;
+//   print(c);
+// }
+
+// // A function with four named optional arguments that returns the multiplication of all arguments.
+// int function3({int num1=1, int num2=1, int num3=1, int num4=1}) {
+//   return (num1*num2*num3*num4);
+// }
+
+// // A function with one argument that prints your name.
+// void printName({String name}) {
+//   print(name);
+// }
+
